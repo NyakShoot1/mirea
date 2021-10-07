94506c74c94fd03c7686aceedd53e19f78e61102
NyakShoot1 <79357728+NyakShoot1@users.noreply.github.com>
2021-10-07 19:49:51 +0300

    Add files via upload

---
+++ praktika5/recursion.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class recursion {
+
+    void task2(int n, int N)
+    {
+        System.out.println(N);
+        if(N < n)
+        {
+            task2(n, N+1);
+        }
+    }
+
+    int task5(int N)
+    {
+        if (N/10!=0)
+            return ((N % 10) + task5(N/10));
+        else
+            return N % 10;
+    }
+
+    public String task3(int firstNum, int secondNum) {
+        if (firstNum > secondNum) {
+            if (firstNum == secondNum)
+                return Integer.toString(firstNum);
+            return firstNum + " " + task3(firstNum - 1, secondNum);
+        } else {
+            if (firstNum == secondNum)
+                return Integer.toString(firstNum);
+            return firstNum + " " + task3(firstNum + 1, secondNum);
+        }
+    }
+
+    public static void main(String[] args) {
+        recursion rec = new recursion();
+        rec.task2(100, 1);
+        System.out.println('\n');
+        System.out.println(rec.task5(3241));
+        System.out.println('\n');
+        System.out.println(rec.task3(30, 20));
+    }
+}
